d745d140cf420dc915a0e866f571c46ea851aa9d
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 15:50:53 -0400

    Solved: Pattern 2 (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Pattern_2.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Pattern 2
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+class Solution {
+public:
+    void pattern2(int n) {
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){ 
+        cout<<"*";
+
+    }
+    cout<<endl;
+}
+    }
+};
