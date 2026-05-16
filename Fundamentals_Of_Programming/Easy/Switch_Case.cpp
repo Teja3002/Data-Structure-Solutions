30d6b11254a4e3f7ca9108e98e9f284fe8a3a1b5
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 15:36:10 -0400

    Solved: Switch Case (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Switch_Case.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: Switch Case
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+class Solution {
+public:
+    void whichWeekDay(int day) {
+        switch(day){
+case 1:
+cout<<"Monday";
+break;
+
+case 2:
+cout<<"Tuesday";
+break;
+
+case 3:
