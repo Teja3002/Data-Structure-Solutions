b754e6123ca8cd46e9f1ac212a7af7d07a4a036e
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 16:06:44 -0400

    Solved: Pattern 11 (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Pattern_11.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Pattern 11
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+value = 0;
+            }
+
+            for (int j = 1; j <= i; j++) {
+                cout << value << " ";
+                value = 1 - value;
+            }
+
+            cout << endl;
+        }
+    }
+};
