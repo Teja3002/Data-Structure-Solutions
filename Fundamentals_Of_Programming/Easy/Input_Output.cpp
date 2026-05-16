6974cf189be4ce48eb87fc53f824541229992bda
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 15:32:15 -0400

    Solved: Input Output (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Input_Output.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: Input Output
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+class Solution {
+public:
+    void printNumber() {
+ int num;
+ cin>>num;
+ cout<<num<<endl;
+ 
+    }
+};
