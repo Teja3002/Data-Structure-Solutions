d64d96f0763333bd2fdca3876a35cf27dadc4408
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 18:10:11 -0400

    Solved: Pattern 15 (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Pattern_15.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void pattern15(int n) {
+       for(int i=1;i<=n;i++){
+
+        for(char txt ='A';txt<='A'+(n-i);txt++){
+       } 
+            cout<<txt;
+        }
+    }
+        cout<<endl;
+};
