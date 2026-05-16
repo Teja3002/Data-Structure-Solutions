7a7d30c6d8ba3ed875a8197ad22e4ef2a1b53dc6
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 15:50:37 -0400

    Solved: Pattern 1 (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Pattern_1.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Pattern 1
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+class Solution {
+public:
+    void pattern1(int n) {
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+    cout<<"*";
+    }
+    cout<<endl;
+}
+    }
+};
