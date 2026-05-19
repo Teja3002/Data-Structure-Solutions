044751a2807a3aae111747714a0213fcc08a357b
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-18 23:06:48 -0400

    Solved: Pattern 20 (Medium) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Medium/Pattern_20.cpp
@@ -0,0 +1,13 @@
+for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
