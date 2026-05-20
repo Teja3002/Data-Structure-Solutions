7207d6cc27e5b0cbf5722345748f3d37ebf317e3
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-19 22:00:51 -0400

    Solved: GCD of Two Numbers (Easy) - Fundamentals Of Programming

--- Fundamentals_Of_Programming/Easy/GCD_of_Two_Numbers.cpp
+++ Fundamentals_Of_Programming/Easy/GCD_of_Two_Numbers.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int GCD(int n1,int n2) {
-int div=1;
-for(int i=1;i<=min(n1,n2);i++){
-    if(n1%i==0 && n2%i==0){
-        div++;
-    }
-    return div;
-}
+    int GCD(int n1, int n2) {
+        int div = 1;
+
+        for (int i = 1; i <= min(n1, n2); i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                div = i;
+            }
+        }
+
+        return div;
     }
 };
