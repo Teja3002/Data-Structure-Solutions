90797d3c14f60e9abea53c450f3abcfa63c252f3
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-31 18:19:15 -0400

    Solved: Two Sum (Easy) - Sorting

---
+++ Sorting/Easy/Two_Sum.cpp
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(n):
+            for j in range(i+1,n):
+        n=len(nums)
+                if (nums[i]+nums[j]==target):
+                    return ans
+        return[-1,-1]
+        ans=[0,0]
+                    ans[0]=i
+                    ans[1]=j
