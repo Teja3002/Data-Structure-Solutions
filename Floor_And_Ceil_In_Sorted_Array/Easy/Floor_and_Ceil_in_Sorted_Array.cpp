42c5a96564e52472e9b21edd409511d1863c2419
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-06-01 12:00:20 -0400

    Solved: Floor and Ceil in Sorted Array (Easy) - Floor And Ceil In Sorted Array

--- Floor_And_Ceil_In_Sorted_Array/Easy/Floor_and_Ceil_in_Sorted_Array.cpp
+++ Floor_And_Ceil_In_Sorted_Array/Easy/Floor_and_Ceil_in_Sorted_Array.cpp
@@ -1,12 +1,13 @@
-mid=(low+high)//2
+class Solution:
+    def getFloorAndCeil(self, nums, x):
+        n=len(nums)
+        low=0
+        high=n-1
+        ciel=-1
+        floor=-1
+
+        while low<=high:
+            mid=(low+high)//2
 
             if nums[mid] == x:
                 return nums[mid], nums[mid]
-                floor=nums[mid]
-                low=mid+1
-            else:
-                ciel=nums[mid]
-                high=mid-1
-                
-            elif nums[mid]<x:
-        return floor,ciel
