21b17df3dec6f8e8ddbd5684cc14f35244b7a2a2
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-06-02 10:08:45 -0400

    Solved: Search in rotated sorted array-I (Medium) - Search In Rotated Sorted Array I

--- Search_In_Rotated_Sorted_Array_I/Medium/Search_in_rotated_sorted_array-I.cpp
+++ Search_In_Rotated_Sorted_Array_I/Medium/Search_in_rotated_sorted_array-I.cpp
@@ -1,35 +1,7 @@
-class Solution:
-    # Function to search for the target element in a rotated sorted array
-    def search(self, nums, target):
-        low, high = 0, len(nums) - 1
-
-        # Applying binary search algorithm
+# Applying binary search algorithm
         while low <= high:
             mid = (low + high) // 2
 
             # Check if mid points to the target
             if nums[mid] == target:
                 return mid
-
-                                                                                                                                                                          
-                                                                                                                                                                          
-                                                                                                                                                                          
-                                                                                                                                                                          
-                                                                                                                                                                          
-                                                                                                                                                                          
-                                                                                                                                                                          
-                                               # Check if the left part is sorted
-            if nums[low] <= nums[mid]:
-                if nums[low] <= target <= nums[mid]:
-                    # Target exists in the left sorted part
-                    high = mid - 1
-                else:
-                    # Target does not exist in the left sorted part
-                    low = mid + 1
-            else:
-                # Check if the right part is sorted
-                if nums[mid] <= target <= nums[high]:
-                    # Target exists in the right sorted part
-                    low = mid + 1
-                else:
-                    # Target does not exist in the right sorted part
