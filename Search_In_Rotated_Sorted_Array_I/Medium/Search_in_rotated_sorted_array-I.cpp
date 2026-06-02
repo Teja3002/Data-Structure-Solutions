18cadbba9457cc578b8ccedc9ee4676df413d2e5
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-06-02 09:33:48 -0400

    Solved: Search in rotated sorted array-I (Medium) - Search In Rotated Sorted Array I

--- Search_In_Rotated_Sorted_Array_I/Medium/Search_in_rotated_sorted_array-I.cpp
+++ Search_In_Rotated_Sorted_Array_I/Medium/Search_in_rotated_sorted_array-I.cpp
@@ -1,5 +1,5 @@
 class Solution:
-    # Function to search for the target element in a rotated sorted 
+    # Function to search for the target element in a rotated sorted array
     def search(self, nums, target):
         low, high = 0, len(nums) - 1
 
@@ -7,7 +7,26 @@ class Solution:
         while low <= high:
             mid = (low + high) // 2
 
-    array
             # Check if mid points to the target
             if nums[mid] == target:
                 return mid
+
+            # Check if the left part is sorted
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    # Target exists in the left sorted part
+                    high = mid - 1
+                else:
+                    # Target does not exist in the left sorted part
+                    low = mid + 1
+            else:
+                # Check if the right part is sorted
+                if nums[mid] <= target <= nums[high]:
+                    # Target exists in the right sorted part
+                    low = mid + 1
+                else:
+                    # Target does not exist in the right sorted part
+                    high = mid - 1
+
+        # If target is not found
+        return -1
