8e655b9f3eeec5171e5cd504d41246d13ad189e7
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-06-02 09:14:01 -0400

    Solved: Search in rotated sorted array-I (Medium) - Search In Rotated Sorted Array I

--- Search_In_Rotated_Sorted_Array_I/Medium/Search_in_rotated_sorted_array-I.cpp
+++ Search_In_Rotated_Sorted_Array_I/Medium/Search_in_rotated_sorted_array-I.cpp
@@ -2,8 +2,6 @@ class Solution:
     def search(self, nums, k):
         for i in range(len(nums)):
             if nums[i]==k:
+                return i
             
         return -1
-        
-
-                return i
