7d19082ff1a10c2ca48cf42aea3b306e1ed46930
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-06-02 09:55:37 -0400

    Solved: Search in rotated sorted array-I (Medium) - Search In Rotated Sorted Array I

--- Search_In_Rotated_Sorted_Array_I/Medium/Search_in_rotated_sorted_array-I.cpp
+++ Search_In_Rotated_Sorted_Array_I/Medium/Search_in_rotated_sorted_array-I.cpp
@@ -10,23 +10,3 @@ class Solution:
             # Check if mid points to the target
             if nums[mid] == target:
                 return mid
-
-            # Check if the left part is sorted
-            if nums[low] <= nums[mid]:
-                if nums[low] <= target <= nums[mid]:
-                    # Target exists in the left sorted part
-                    high = mid - 1
-                else:
-                    # Target does not exist in the left sorted part
-                    low = mid + 1
-            else:
-                # Check if the right part is sorted
-                if nums[mid] <= target <= nums[high]:
-                    # Target exists in the right sorted part
-                    low = mid + 1
-                else:
-                    # Target does not exist in the right sorted part
-                    high = mid - 1
-
-        # If target is not found
-        return -1
